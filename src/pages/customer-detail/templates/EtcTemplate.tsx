caab46c5f4875e75964d49ce86e77292639d4aaa
장현웅 <jhw811@gmail.com>
2021-08-06 14:44:12 +0900

    바코드 넣기 완료

--- src/pages/customer-detail/templates/EtcTemplate.tsx
+++ src/pages/customer-detail/templates/EtcTemplate.tsx
@@ -1,23 +1,38 @@
-import { CustomerDetailData } from '@store/customer';
+import { CustomerDetailBizData, CustomerDetailData } from '@store/customer';
 import { Descriptions } from 'antd';
 import React, { FC } from 'react';
+import { useBarcode } from 'react-barcodes';
+import styled from 'styled-components';
 
 interface EtcTemplateInterface {}
 
 const EtcTemplate: FC<EtcTemplateInterface> = () => {
   const { customerDetail } = CustomerDetailData('123');
+  const { customerBizDetail } = CustomerDetailBizData('123');
 
-  console.log('customerDetail', customerDetail);
+  const { inputRef } = useBarcode({
+    value: customerBizDetail?.bizNo ?? '',
+    options: {},
+  });
+
   return (
     <Descriptions bordered column={{ xxl: 3, xl: 3, lg: 3, md: 3, sm: 2, xs: 1 }}>
       <Descriptions.Item label='Route Code'>{customerDetail?.routeCode}</Descriptions.Item>
-      <Descriptions.Item label='사업자번호(Barcode)'>{customerDetail?.bizNo}</Descriptions.Item>
-      <Descriptions.Item label='징수형태'>{customerDetail?.accntNo}</Descriptions.Item>
-      <Descriptions.Item label='관세납부방법'>{customerDetail?.accntNo}</Descriptions.Item>
-      <Descriptions.Item label='대납정보'>{customerDetail?.accntNo}</Descriptions.Item>
-      <Descriptions.Item label='HAWB No.'>{customerDetail?.accntNo}</Descriptions.Item>
+      <Descriptions.Item label='사업자번호(Barcode)'>
+        <BarcodeImage ref={inputRef} />
+      </Descriptions.Item>
+      <Descriptions.Item label='징수형태'>{customerBizDetail?.collectType}</Descriptions.Item>
+      <Descriptions.Item label='관세납부방법'>{customerBizDetail?.taxPayMethod}</Descriptions.Item>
+      <Descriptions.Item label='대납정보'>{customerBizDetail?.taxProxyAmt}</Descriptions.Item>
+      <Descriptions.Item label='HAWB No.'>{''}</Descriptions.Item>
     </Descriptions>
   );
 };
+
+const BarcodeImage = styled.canvas`
+  flex: 1;
+  width: 100%;
+  height: 100px;
+`;
 
 export default EtcTemplate;
